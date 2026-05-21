e3e3e8c3569a6660a6ee98389b945a954def0c70
Caio Rosa <caioleonel01@gmail.com>
2026-05-21 17:36:28 -0300

    Use registry for weapon firing

--- project/scripts/weapons/WeaponFire.gd
+++ project/scripts/weapons/WeaponFire.gd
@@ -1,36 +1,86 @@
 extends RefCounted
 
+static var FIRE_CONFIG = {
+	"revolver": {
+		"family": "revolver",
+		"color": Color("#ffe7a0"),
+		"bonus_shots": 0
+	},
+	"shotgun": {
+		"family": "shotgun",
+		"bonus_pellets": 0
+	},
+	"dynamite": {
+		"family": "explosive",
+		"visual": "dynamite"
+	},
+	"lasso": {
+		"family": "lasso"
+	},
+	"knife": {
+		"family": "knife"
+	},
+	"rifle": {
+		"family": "rifle",
+		"color": Color("#d8e4ff"),
+		"visual": "rail"
+	},
+	"fire_bottle": {
+		"family": "explosive",
+		"visual": "fire"
+	},
+	"horseshoe": {
+		"family": "horseshoe"
+	},
+	"golden_revolver": {
+		"family": "revolver",
+		"color": Color("#ffd55d"),
+		"bonus_shots": 2
+	},
+	"coach_gun": {
+		"family": "shotgun",
+		"bonus_pellets": 3
+	},
+	"rail_spike": {
+		"family": "rifle",
+		"color": Color("#b9f0ff"),
+		"visual": "rail_spike"
+	},
+	"ghost_lantern": {
+		"family": "ghost_lantern"
+	}
+}
+
 static func fire(context, weapon_id, shooter):
 	if not context._player_alive(shooter):
 		return
 
+	var config = FIRE_CONFIG.get(weapon_id, {})
+	if config.is_empty():
+		return
+
 	context.firing_weapon_context = weapon_id
-	match weapon_id:
+	_fire_from_config(context, weapon_id, shooter, config)
+	context.firing_weapon_context = ""
+
+static func _fire_from_config(context, weapon_id, shooter, config):
+	match str(config.get("family", "")):
 		"revolver":
-			_fire_revolver(context, weapon_id, shooter, Color("#ffe7a0"), 0)
+			_fire_revolver(context, weapon_id, shooter, config.get("color", Color("#ffe7a0")), int(config.get("bonus_shots", 0)))
 		"shotgun":
-			_fire_shotgun(context, weapon_id, shooter, 0)
-		"dynamite":
-			_fire_explosive(context, weapon_id, shooter, "dynamite")
+			_fire_shotgun(context, weapon_id, shooter, int(config.get("bonus_pellets", 0)))
+		"explosive":
+			_fire_explosive(context, weapon_id, shooter, str(config.get("visual", "dynamite")))
 		"lasso":
 			_fire_lasso(context, weapon_id, shooter)
 		"knife":
 			_fire_knife(context, weapon_id, shooter)
 		"rifle":
-			_fire_rifle(context, weapon_id, shooter, Color("#d8e4ff"), "rail")
-		"fire_bottle":
-			_fire_explosive(context, weapon_id, shooter, "fire")
+			_fire_rifle(context, weapon_id, shooter, config.get("color", Color("#d8e4ff")), str(config.get("visual", "rail")))
 		"horseshoe":
 			_fire_horseshoe(context, weapon_id, shooter)
-		"golden_revolver":
-			_fire_revolver(context, weapon_id, shooter, Color("#ffd55d"), 2)
-		"coach_gun":
-			_fire_shotgun(context, weapon_id, shooter, 3)
-		"rail_spike":
-			_fire_rifle(context, weapon_id, shooter, Color("#b9f0ff"), "rail_spike")
 		"ghost_lantern":
 			_fire_ghost_lantern(context, weapon_id, shooter)
-	context.firing_weapon_context = ""
 
 static func _fire_revolver(context, weapon_id, shooter, color, bonus_shots):
 	var base_direction = context._weapon_direction(weapon_id, shooter)
